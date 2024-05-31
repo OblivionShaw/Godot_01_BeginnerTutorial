9f6075aac615c624bc153acb10446c21e6599b6b
OblivionShaw <sh960208@gmail.com>
2024-05-31 18:57:56 +0800

    feat:change_scene_to_file

--- FirstGame/scripts/game_manager.gd
+++ FirstGame/scripts/game_manager.gd
@@ -8,3 +8,8 @@ func add_point():
 	score +=1;
 	print(score)
 	score_label.text = "超爽的 撿到" + str(score) +"塊"
+
+
+func _on_portal_area_body_entered(body):
+	get_tree().change_scene_to_file("res://scenes/game2.tscn")
+	pass # Replace with function body.
